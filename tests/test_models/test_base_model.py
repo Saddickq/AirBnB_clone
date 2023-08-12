5ac29192c81feb850ffb295bf86e3a3e4a0e8824
Ardiy <you@example.com>
2023-08-12 20:24:38 +0000

    user.py and some pycode style adjustsments and test package

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,38 @@
+import unittest
+from uuid import UUID
+import datetime
+
+class TestBaseModel(unittest.TestCase):
+    def test_instance_creation(self):
+        person = BaseModel(name="Saddiq", my_number=30)
+        self.assertEqual(person.name, "Saddiq")
+        self.assertEqual(person.my_number, 30)
+
+    def test_default_values(self):
+        person = BaseModel(name="Ardiy", my_number=25)
+        self.assertEqual(person.name, "Ardiy")
+        self.assertEqual(person.my_number, 25)
+
+    def test_my_number_is_integer(self):
+        with self.assertRaises(ValueError):
+            my_model = BaseModel(name="Bob", my_number="thirty")
+
+    def test_id_is_uuid(self):
+        my_model = BaseModel(name="Saddiq", my_number=28)
+        self.assertIsInstance(my_model.id, UUID)
+
+    def test_created_at_date(self):
+        my_model = BaseModel(name="Michael", my_number=35)
+        self.assertIsInstance(my_model.created_at, datetime.datetime)
+    
+    def test_updated_at_date(self):
+        my_model = BaseModel(name="Michael", my_number=35)
+        self.assertIsInstance(my_model.updated_at, datetime.datetime)
+    
+    def test_uuid_is_string(self):
+        my_model = BaseModel(name="Saddiq", age=30)
+        self.assertIsInstance(str(my_model.id), str)
+
+
+if __name__ == '__main__':
+    unittest.main()
